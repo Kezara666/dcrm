f202ec2a16881440ebfd9df9c9638d3a71ada241
Kezara Lakshan <kezaralakshan666@gmail.com>
2023-04-08 18:28:40 +0530

    adding
    added files git

---
+++ mydb.py
@@ -0,0 +1,10 @@
+import mysql.connector
+database=mysql.connector.connect(
+    host='localhost',
+    user='root',
+    passwd='1234'
+)
+
+cursorObject=database.cursor()
+cursorObject.execute("CREATE DATABASE elderco")
+print("alldone")
